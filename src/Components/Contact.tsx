a78791d6d9b3ed242c296ca5a0b07d900ee3fc5a
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-08-09 21:45:27 +0900

    Add: Contact Link

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import Link from 'react-scroll';
+import { Typography, Box, Button} from '@mui/material';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+
 
 const Contact: React.FC = () => {
-        return (
-            <div className="Contact">   
-            </div>
+    return (
+        <>
+            <Box sx={{display:"flex",justifyContent:"center"}}>
+                <Typography variant="h4" sx={{textAlign:"center", fontSize: "24px", marginTop:"80px"}}>
+                    Contact
+                </Typography>
+            </Box>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <Button target="_blank" href="https://twitter.com/tsuttsun_kk">
+                    <TwitterIcon style={{ width: "50px", height: "50px", top: "-10px", margin: "20px" }} />
+            </Button>
+            <Button target="_blank" href="https://github.com/wind111-lang">
+                    <GitHubIcon style={{ width: "50px", height: "50px", top: "-10px", margin: "20px" }} />
+                </Button>
+                <Button target="_blank" href="mailto:k20076kk@aitech.ac.jp">
+                <EmailIcon style={{ width: "50px", height: "50px", top: "-10px", margin: "20px" }} />
+                </Button>
+            </Box>
+        </>
         )
 }
 
