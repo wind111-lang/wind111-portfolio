afacd09f2917f99820e4daa3bf37e3690008f76c
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-08-09 21:45:54 +0900

    Fix: Style fixed

--- src/Components/About.tsx
+++ src/Components/About.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import Link from 'react-scroll';
-import{Box,Avatar,Typography,Paper} from '@mui/material'
+import { Box, Avatar, Typography, Paper } from '@mui/material'
 
 import Image from "./me.jpg"
 
 const About: React.FC = () =>{
     return (
         <>
-            <Box>
-                <Typography variant="h4" sx={{marginTop:"20px", marginLeft:"100px", fontSize:"50px"}}>
+            <Typography variant="h4" sx={{textAlign:"center", marginTop:"30px"}}>
                     About me
                 </Typography>
+            <Box sx={{display:"flex",justifyContent:"center"}}>
                 <Avatar
                     alt="Tsutsui Shota" src={Image}
-                    style={{ width: "300px", height: "300px", top: "-10px", left: "400px", margin: "20px" }}
+                    style={{ width: "300px", height: "300px", top: "50px", right: "90px", margin: "20px" }}
                 />
-                <Typography sx={{fontSize: '48px', textAlign: "center", marginTop: "-250px", marginRight: "-300px", position: "relative"}}>
+                <Typography sx={{ fontSize: '48px', textAlign: "center", marginTop: "160px", marginLeft:"20px"}}>
                     Tsutsui Shota
-                </Typography>
-                <Typography sx={{fontSize: '28px', textAlign: "center", marginTop: "-5px", marginRight: "-300px", position: "relative"}}>
+                    <Typography sx={{fontSize: '28px', textAlign:"center", marginRight:"0px"}}>
                     University Student
+                    </Typography>
                 </Typography>
             </Box>
         </>
