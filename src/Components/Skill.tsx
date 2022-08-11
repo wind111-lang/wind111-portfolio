42b0115026b5af09b2ab842b8d8badfc9dad82e4
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-08-11 22:14:19 +0900

    Add: skills

--- src/Components/Skill.tsx
+++ src/Components/Skill.tsx
@@ -1,12 +1,29 @@
 import React from 'react';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import Link from 'react-scroll';
+import { Box, Grid, Typography} from '@mui/material';
+import Image from "./coding.jpg";
 
 const Skill: React.FC = () => {
-
+const skills = ['C','C#','Python','Java','Go','Gin','Gorm','Unity','Docker','Git','MariaDB']
         return (
-            <div className="Skill">   
-            </div>
+            <>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={8} lg={6}>
+                        <Box sx={{ textAlign: 'center'}}>
+                            <Typography variant="h4" sx={{marginTop:'30px'}}>
+                                Skills
+                            </Typography>
+                            {skills.map((skill) =>
+                                <Typography sx={{ margin:'1em'}}>
+                                    {skill}
+                                </Typography>
+                            )}
+                        </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6} lg={6}>
+                        <Box component='img' alt='skill' src={Image} sx={{width:'500px', height:'500px'}}></Box>
+                    </Grid>
+                </Grid>
+            </>
         )
 }
 
