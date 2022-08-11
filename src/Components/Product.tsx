ceb4e8ced985750d133261c1985770aae0f6ccbb
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-08-11 22:14:37 +0900

    Fix: import

--- src/Components/Product.tsx
+++ src/Components/Product.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Layout, Header, Navigation, Drawer, Content} from 'react-mdl';
-import Link from 'react-scroll';
+
 
 const Product: React.FC = () => {
 
         return (
             <>
+                
             </>
         )
 }
