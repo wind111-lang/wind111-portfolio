c61b03ac623154ed52a5cd88512740dcec79bb3f
wind111-lang <k20076kk@aitech.ac.jp>
2022-08-15 00:13:44 +0900

    Fix: styles,del mediaquery

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,6 @@ import Contact from './Components/Contact';
 import Nav from './Components/Nav';
 import Skill from './Components/Skill';
 import Product from './Components/Product';
-import MediaQuery from 'react-responsive/';
 
 const App: React.FC = () => {
     const about = React.useRef(null);
@@ -15,10 +14,8 @@ const App: React.FC = () => {
     const skill = React.useRef(null);
     const product = React.useRef(null);
     const contact = React.useRef(null);
-
         return (
             <>
-            <MediaQuery query="(max-width: 400px)">
                 <section>
                     <Nav profile={profile} about={about} skill={skill} product={product} contact={contact} />
                 </section>
@@ -36,28 +33,7 @@ const App: React.FC = () => {
                 </section>
                 <section ref={contact}>
                     <Contact />
-                    </section>
-            </MediaQuery>
-            <MediaQuery query="(min-width: 401px)">
-                <section>
-                    <Nav profile={profile} about={about} skill={skill} product={product} contact={contact} />
                 </section>
-                <section ref={profile}>
-                    <Profile />
-                </section>
-                <section ref={about}>
-                    <About />
-                </section>
-                <section ref={skill}>
-                    <Skill />
-                </section>
-                <section ref={product}>
-                    <Product />
-                </section>
-                <section ref={contact}>
-                    <Contact />
-                    </section>
-            </MediaQuery>
             </>
         );
 }
