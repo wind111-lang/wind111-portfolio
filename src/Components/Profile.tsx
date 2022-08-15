1f693cfde2fc6a9a110b3b5aa68296627ef26af6
wind111-lang <k20076kk@aitech.ac.jp>
2022-08-15 21:19:42 +0900

    Fix:mediaquery added in avatar icon

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -7,24 +7,30 @@ const Profile: React.FC = () =>{
     return (
         <>
             <Box sx={{ display: "flex", justifyContent: "center" }}>
-            <MediaQuery maxWidth={375}>
-                <Avatar
-                    alt="Tsutsui Shota" src={Image}
-                    style={{ width: "150px", height: "150px", top: "50px", right: "90px", margin: "20px" }}
+                <MediaQuery query='(max-width:400px)'>
+                    <Avatar
+                        alt="Tsutsui Shota" src={Image}
+                        style={{ width: "150px", height: "150px", top: "50px", right: "90px", margin: "20px" }}
                     />
-                </MediaQuery>
-            <MediaQuery minWidth={375}>
-                <Avatar
-                    alt="Tsutsui Shota" src={Image}
-                    style={{ width: "300px", height: "300px", top: "50px", right: "90px", margin: "20px" }}
-                    />
-            </MediaQuery>
-                <Typography sx={{ fontSize: '48px', textAlign: "center", marginTop: "160px", marginLeft:"20px"}}>
-                    Tsutsui Shota
-                    <Typography sx={{fontSize: '28px', textAlign:"center", marginRight:"0px"}}>
-                    University Student
+                    <Typography sx={{ fontSize: '24px', textAlign: "center", marginTop: "160px", marginLeft:"20px"}}>
+                        Tsutsui Shota
                     </Typography>
-                </Typography>
+                    <Typography sx={{fontSize: '14px', textAlign:"center", marginRight:"0px"}}>
+                        University Student
+                    </Typography>
+                </MediaQuery>
+                <MediaQuery query='(min-width:401px)'>
+                    <Avatar
+                        alt="Tsutsui Shota" src={Image}
+                        style={{ width: "300px", height: "300px", top: "50px", right: "90px", margin: "20px" }}
+                    />
+                    <Typography sx={{ fontSize: '48px', textAlign: "center", marginTop: "160px", marginLeft:"20px"}}>
+                        Tsutsui Shota
+                    </Typography>
+                    <Typography sx={{fontSize: '28px', textAlign:"center", marginRight:"0px"}}>
+                        University Student
+                    </Typography>
+                </MediaQuery>
             </Box>
         </>
     );
