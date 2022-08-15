52f9bf7f382e41b42df3d8b6202b192512cf0a83
wind111-lang <k20076kk@aitech.ac.jp>
2022-08-15 21:14:36 +0900

    Fix:mediaquery added in avatar icon

--- src/Components/Profile.tsx
+++ src/Components/Profile.tsx
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Box, Avatar, Typography} from '@mui/material'
-
+import MediaQuery from 'react-responsive';
 import Image from "./me.jpg"
 
 const Profile: React.FC = () =>{
     return (
         <>
-            <Box sx={{display:"flex",justifyContent:"center"}}>
+            <Box sx={{ display: "flex", justifyContent: "center" }}>
+            <MediaQuery maxWidth={375}>
+                <Avatar
+                    alt="Tsutsui Shota" src={Image}
+                    style={{ width: "150px", height: "150px", top: "50px", right: "90px", margin: "20px" }}
+                    />
+                </MediaQuery>
+            <MediaQuery minWidth={375}>
                 <Avatar
                     alt="Tsutsui Shota" src={Image}
                     style={{ width: "300px", height: "300px", top: "50px", right: "90px", margin: "20px" }}
-                />
+                    />
+            </MediaQuery>
                 <Typography sx={{ fontSize: '48px', textAlign: "center", marginTop: "160px", marginLeft:"20px"}}>
                     Tsutsui Shota
                     <Typography sx={{fontSize: '28px', textAlign:"center", marginRight:"0px"}}>
